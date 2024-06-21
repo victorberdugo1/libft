e9c1ce80f4d0ee611134b8b39748f1c0db8ab856
Victor Berdugo <vberdugo@cbr9s2.42barcelona.com>
2024-06-21 11:23:32 +0200

    4

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 11:40:03 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/06/19 17:16:31 by vberdugo         ###   ########.fr       */
+/*   Updated: 2024/06/21 11:17:48 by vberdugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,20 +29,26 @@ int	ft_atoi(const char *nptr)
 			negf = -1;
 		i++;
 	}
-	while (ft_isdigit(nptr[i]) || nptr[i] != 0)
+	if (ft_isdigit(nptr[i]))
 	{
-		res = (res * 10) + (nptr[i] - '0');
-        i++;
+		while (ft_isdigit(nptr[i]) || nptr[i] != 0)
+		{
+			res = (res * 10) + (nptr[i] - '0');
+			i++;
+		}
+		return (res * negf);
 	}
-	return (res * negf);
+	return (0);
 }
 /*
 #include <stdio.h>
 #include <stdlib.h>	
 
-int main (int argc,const char *argv[])
+int main (int argc,char *argv[])
 {
-argc = 0;
-printf("%d",ft_atoi(*argv[1]));
-
+	if(argc != 2)
+		return 1;
+	printf("%d\n",ft_atoi(argv[1]));
+	printf("%d",atoi(argv[1]));
+return 0;
 }*/
