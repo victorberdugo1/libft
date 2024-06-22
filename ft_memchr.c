a3148b59afee6df2979fba1382f97ce721c093e4
Victor <victor@localhost.localdomain>
2024-06-22 11:16:48 +0200

    c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 11:26:52 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/06/21 15:56:22 by vberdugo         ###   ########.fr       */
+/*   Updated: 2024/06/22 11:15:00 by victor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,31 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*z;
+	int				con;
 
-	z = s;
-	return (s);
+	z = (unsigned char *)s;
+	con = 0;
+	while (con < n)
+	{
+		if (*z == (unsigned char)c)
+			return ((void *)z);
+		z++;
+		con++;
+	}
+	return (NULL);
 }
+/*#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    const char str[] = "Hello, world!";
+    const char *ptr = ft_memchr(str, 'w', sizeof(str));
+
+    if (ptr != NULL) {
+        printf("Caracter 'w' encontrado en la posición %ld\n", ptr - str);
+    } else {
+        printf("Caracter 'w' no encontrado\n");
+    }
+
+    return 0;
+}*/
