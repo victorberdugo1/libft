9f35b2c97ea275f685e6d1cb9125a27b7888951e
Victor <victor@localhost.localdomain>
2024-06-22 12:43:43 +0200

    a

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,10 +6,37 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 10:30:52 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/06/19 17:47:26 by vberdugo         ###   ########.fr       */
+/*   Updated: 2024/06/22 12:43:08 by victor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*tmp;
+	size_t			i;
+
+	tmp = (unsigned char *)dest;
+	i = 0;
+	while (i < n)
+	{
+		tmp[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
+/*#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char src[] = "Hello, world!";
+    char dest_ft[20];
+    char dest_memcpy[20];
+
+    ft_memcpy(dest_ft, src, strlen(src) + 1);
+    memcpy(dest_memcpy, src, strlen(src) + 1);
+    printf("Cadena copiada usando ft_memcpy: %s\n", dest_ft);
+    printf("Cadena copiada usando memcpy    : %s\n", dest_memcpy);
+    return 0;
+}*/
