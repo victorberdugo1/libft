ac11470f6cf51ae70f232dcb288d59679fbbb137
Victor <victor@localhost.localdomain>
2024-06-22 11:43:48 +0200

    here

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,10 +6,47 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 11:36:48 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/06/18 11:37:50 by vberdugo         ###   ########.fr       */
+/*   Updated: 2024/06/22 11:42:35 by victor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	int				i;
+	unsigned char	*p1;
+	unsigned char	*p2;
+
+	p1 = (unsigned char *)s1;
+	p2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (p1[i] != p2[i])
+			return (p1[i] - p2[i]);
+		i++;
+	}
+	return (0);
+}
+/*#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[] = "Hello";
+    char str2[] = "Hello";
+    char str3[] = "World";
+    // Comparar str1 y str2
+    if (memcmp(str1, str2, sizeof(str1)) == 0) {
+        printf("str1 y str2 son iguales\n");
+    } else {
+        printf("str1 y str2 son diferentes\n");
+    }
+    // Comparar str1 y str3
+    if (memcmp(str1, str3, sizeof(str1)) == 0) {
+        printf("str1 y str3 son iguales\n");
+    } else {
+        printf("str1 y str3 son diferentes\n");
+    }
+    return 0;
+}*/
